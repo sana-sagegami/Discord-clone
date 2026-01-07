507e8951dc1b1964b8beb5f46ac8e3b38e884ce4
sana-sagegami <sana01237@icloud.com>
2026-01-07 22:02:42 +0900

    Firebase設定を環境変数を使用するように更新

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBK6pGNnwMtsk24m19qVDweDnVQo9WYm9o",
-  authDomain: "discord-clone-a1eee.firebaseapp.com",
-  projectId: "discord-clone-a1eee",
-  storageBucket: "discord-clone-a1eee.firebasestorage.app",
-  messagingSenderId: "120076068210",
-  appId: "1:120076068210:web:5ab024be6b6dbfcc4069eb",
-  measurementId: "G-TEH7KSTSR7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID!,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
